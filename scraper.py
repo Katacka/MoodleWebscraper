ceffebb696044185ad5df9e609363b6f6ed8def5
Alex Hadi <alexhisb@gmail.com>
2020-03-09 21:21:16 -0700

    Changes primarily to formatting. Added type hints to all functions.

--- scraper.py
+++ scraper.py
@@ -1,268 +1,276 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException, ElementNotVisibleException, NoAlertPresentException
+import os
+import re
+import shutil
+import string
 import sys
 import time
-import os
-import string
-import re
 import urllib.request
-import shutil
+from typing import Dict, List, Tuple, Iterator
 
-class Course():
-    def __init__(self, name, url):
-        self.name = name
+from selenium import webdriver
+from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException, ElementNotVisibleException
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class Course:
+    def __init__(self, course_name: str, url: str):
+        self.course_name = course_name
         self.url = url
         self.file_groups = {}
         self.html = None
 
-## Helper Methods ##
 
-def setupDriver(load_speed):
-    #Ensure directory for downloaded files exists
+# region Helper Methods
+
+def setup_chrome_web_driver(load_speed: int) -> WebDriver:
+    # Ensure directory for downloaded files exists
     if not os.path.isdir("files"):
         os.mkdir("files")
-        
-    #Create browser web driver
+
+    # Create browser web driver
     options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--safebrowsing-disable-extension-blacklist')
-    options.add_argument('--safebrowsing-disable-download-protection')
-    prefs = {"download.default_directory": os.path.dirname(os.path.abspath(__file__)) + "/files",
-             'download.prompt_for_download': False,
-             'safebrowsing.enabled': True,
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--incognito")
+    options.add_argument("--headless")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--safebrowsing-disable-extension-blacklist")
+    options.add_argument("--safebrowsing-disable-download-protection")
+    prefs = {
+        "download.default_directory": os.path.dirname(os.path.abspath(__file__)) + "/files",
+        "download.prompt_for_download": False,
+        "safebrowsing.enabled": True
     }
     options.add_experimental_option("prefs", prefs)
 
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.implicitly_wait(load_speed)
-    return driver
+    chrome_web_driver = webdriver.Chrome(chrome_options=options)
+    chrome_web_driver.implicitly_wait(load_speed)
+    return chrome_web_driver
 
 
-def queryMoodle(driver):
-    url = "https://learning.up.edu/moodle"
-    visitPage(driver, url)
+def query_moodle(web_driver: WebDriver) -> None:
+    visit_page(web_driver, "https://learning.up.edu/moodle")
 
 
-def loginMoodle(driver, load_speed):
-    #Login if necessary
+def login_to_moodle(web_driver: WebDriver, load_speed: int) -> None:
+    # Login if necessary
     try:
-        #Enter email/username
-        email_input = driver.find_element_by_id("i0116")
+        # Enter email/username
+        email_input = web_driver.find_element_by_id("i0116")
         email_input.send_keys(sys.argv[1])
-        driver.find_element_by_id("idSIButton9").click()
+        web_driver.find_element_by_id("idSIButton9").click()
         time.sleep(load_speed)
 
-        #Enter password
-        pass_input = driver.find_element_by_id("i0118")
+        # Enter password
+        pass_input = web_driver.find_element_by_id("i0118")
         pass_input.send_keys(sys.argv[2])
-        driver.find_element_by_id("idSIButton9").click()
+        web_driver.find_element_by_id("idSIButton9").click()
         time.sleep(load_speed)
 
-        #Navigate off login page
-        driver.find_element_by_id("idSIButton9").click()
-    
+        # Navigate off login page
+        web_driver.find_element_by_id("idSIButton9").click()
+
     except IndexError:
-        print("ERROR: Improper CLI format\nPlease use: 'scraper.py <email@up.edu> <password>'");
-        driver.quit()
+        print("ERROR: Improper CLI format")
+        print("Please use: 'scraper.py <email@up.edu> <password>'")
+        web_driver.quit()
         exit()
-    
+
     except NoSuchElementException:
-        pass #Login not required..? Debug with non-headless browser if errors propogate
+        pass  # Login not required..? Debug with non-headless browser if errors propagate
 
 
-def scrapeCourseMetadata(driver, load_speed):
-    #Generate list of course meta-data
+def get_dict_entries_for_courses(courses: List[WebElement]) -> Dict[str, Course]:
+    course_dict = {}
+
+    for course in courses:
+        if not course.text:
+            continue
+
+        course_name = format_course_name(course.text)
+        course_dict[course_name] = Course(course_name, course.get_attribute("href"))
+
+    return course_dict
+
+
+def scrape_course_metadata(web_driver: WebDriver, load_speed: int) -> Dict[str, Course]:
+    # Generate list of course metadata
+    course_dict = {}
     try:
-        #Set course overview to display all courses
-        driver.find_element_by_id("groupingdropdown").click()
-        course_overview = driver.find_element_by_id("inst279425")
+        # Set course overview to display all courses
+        web_driver.find_element_by_id("groupingdropdown").click()
+        course_overview = web_driver.find_element_by_id("inst279425")
         course_overview.find_element_by_xpath(".//a[@data-value='all']").click()
 
-        #Scrape course meta-data
-        course_dict = {}
-        while (True):
-            course_overview = driver.find_element_by_id("inst279425")        
+        # Scrape course metadata
+        while True:
+            course_overview = web_driver.find_element_by_id("inst279425")
             courses = course_overview.find_elements_by_class_name("coursename")
-            course_dict = getCourseDict(courses, course_dict)
-        
+            course_dict.update(get_dict_entries_for_courses(courses))
+
             next_page = course_overview.find_element_by_xpath(".//li[@data-control='next']")
-            if (next_page.get_attribute("aria-disabled") == "true"):
+            if next_page.get_attribute("aria-disabled") == "true":
                 break
 
             next_page.click()
             time.sleep(load_speed)
 
     except NoSuchElementException:
-        print("ERROR: Unexpected course overview format\n")
-        driver.quit()
+        print("ERROR: Unexpected course overview format")
+        web_driver.quit()
         exit()
 
     return course_dict
 
 
-def getCourseDict(courses, course_dict):
-    for course in courses:
-        if not course.text:
-            continue
-        
-        course_name = formatCourseName(course.text)
-        course_dict[course_name] = Course(course_name, course.get_attribute("href"))
-    return course_dict
-
-
-def downloadCourseData(driver, course_dict):
-    #Traverse courses
+def download_course_data(web_driver: WebDriver, course_dict: Dict[str, Course]):
+    # Traverse courses
     for course_name in course_dict:
         try:
-            #Visit course page
+            # Visit course page
             course = course_dict[course_name]
-            visitPage(driver, course.url)
-        
-            #Traverse course file groups
-            assignments = driver.find_elements_by_class_name("assign")
-            resources = driver.find_elements_by_class_name("resource")
+            visit_page(web_driver, course.url)
 
-            assignment_files = scrapeAssignments(assignments)
-            resource_files = scrapeResources(resources)
+            # Traverse course file groups
+            assignments = web_driver.find_elements_by_class_name("assign")
+            resources = web_driver.find_elements_by_class_name("resource")
 
-            #Update course data
-            course = downloadAssignments(driver, course, assignment_files)
-            course_dict[course_name] = downloadResources(driver, course, resource_files)
+            assignment_files = scrape_assignments(assignments)
+            resource_files = scrape_resources(resources)
+
+            # Update course data
+            course = download_assignments(web_driver, course, assignment_files)
+            course_dict[course_name] = download_resources(course, resource_files)
 
         except NoSuchElementException:
-            print("ERROR: Unexpected course assignment format\n")
-            driver.quit()
+            print("ERROR: Unexpected course assignment format")
+            web_driver.quit()
             exit()
 
     return course_dict
-    
 
-def scrapeAssignments(files):
-    file_names = [f.find_element_by_xpath(".//span[@class='instancename']").text
-                  for f in files]
+
+def scrape_assignments(files: List[WebElement]) -> Iterator[Tuple[str, str]]:
+    file_names = [f.find_element_by_xpath(".//span[@class='instancename']").text for f in files]
     file_urls = [f.find_element_by_xpath(".//a").get_attribute("href") for f in files]
     return zip(file_names, file_urls)
 
-def downloadAssignments(driver, course, assignments):
+
+def download_assignments(web_driver: WebDriver, course: Course, assignments: Iterator[Tuple[str, str]]):
     for assign_name, assign_url in assignments:
-        assign_name = formatDefault(assign_name)
-                
-        visitPage(driver, assign_url)
-        course.html = driver.page_source
-                
-        #Download files
-        displayHiddenFiles(driver)
-        downloads = driver.find_elements_by_xpath("//div[@class='fileuploadsubmission']/a")
+        assign_name = format_default(assign_name)
+
+        visit_page(web_driver, assign_url)
+        course.html = web_driver.page_source
+
+        # Download files
+        display_hidden_files(web_driver)
+        downloads = web_driver.find_elements_by_xpath("//div[@class='fileuploadsubmission']/a")
 
         download_names = [d.text for d in downloads]
         download_urls = [d.get_attribute("href") for d in downloads]
-        
-        if len(downloads) > 0:
+
+        if downloads:
             course.file_groups[assign_name] = []
             for download_name, url in zip(download_names, download_urls):
-                name = downloadUrl(url, formatDefault(download_name))
+                name = download_url_and_return_file_name(url, format_default(download_name))
                 course.file_groups[assign_name].append(name)
 
     return course
 
 
-def displayHiddenFiles(driver):
-    #Show hidden files
-    expandIcons = driver.find_elements_by_xpath("//a[@aria-expanded='false']")
-    for icon in expandIcons:
+def display_hidden_files(web_driver: WebDriver) -> None:
+    # Show hidden files
+    expand_icons = web_driver.find_elements_by_xpath("//a[@aria-expanded='false']")
+    for icon in expand_icons:
         try:
             icon.click()
-            
         except ElementNotVisibleException:
-            continue #Skip element
+            continue  # Skip element
 
 
-def scrapeResources(files):
-    file_names = [f.find_element_by_xpath(".//span[@class='instancename']").text
-                  for f in files]
-    file_urls = [f.find_element_by_xpath(".//a").get_attribute("href") for f in files]
+def scrape_resources(resources: List[WebElement]) -> Iterator[Tuple[str, str]]:
+    file_names = [f.find_element_by_xpath(".//span[@class='instancename']").text for f in resources]
+    file_urls = [f.find_element_by_xpath(".//a").get_attribute("href") for f in resources]
     return zip(file_names, file_urls)
 
 
-def downloadResources(driver, course, resources):        
+def download_resources(course: Course, resources: Iterator[Tuple[str, str]]) -> Course:
     for resource_name, resource_url in resources:
-        resource_name = formatDefault(resource_name)
+        resource_name = format_default(resource_name)
 
-        name = downloadUrl(resource_url, resource_name)
+        name = download_url_and_return_file_name(resource_url, resource_name)
         course.file_groups[name] = name
 
     return course
 
 
-def downloadUrl(url, file_name, max_attempts=64):
-    allDownloadsFinished()
+def download_url_and_return_file_name(url: str, original_file_name: str, max_attempts: int = 64) -> str:
+    all_downloads_finished()
+
     with urllib.request.urlopen(url) as response:
-        name = file_name
         for i in range(1, max_attempts):
             try:
-                shutil.copyfileobj(response, open("files/" + name, 'wb'))
-                return name
+                with open("files/" + original_file_name, "wb") as files_directory:
+                    shutil.copyfileobj(response, files_directory)
+                return original_file_name
             except FileExistsError:
-                name = file_name + " (" + i + ")"
-        return file_name
+                return f"{original_file_name} ({i})"
 
-                
-def visitPage(driver, url):
+
+def visit_page(web_driver: WebDriver, url: str) -> None:
     try:
-        #Wait for all previous downloads to finish
-        allDownloadsFinished()
-        driver.get(url)
+        # Wait for all previous downloads to finish
+        all_downloads_finished()
+        web_driver.get(url)
 
     except InvalidArgumentException:
         print("ERROR: Malformed URL: ", url)
-        driver.quit()
+        web_driver.quit()
         exit()
 
-        
-def allDownloadsFinished():
-    while (True):
+
+def all_downloads_finished() -> None:
+    while True:
         finished = True
-        for i in os.listdir("files/"):
-            if ".crdownload" in i:
+
+        for file_name in os.listdir("files/"):
+            if ".crdownload" in file_name:
                 finished = False
                 time.sleep(0.5)
-                
+
         if finished:
             break
-        
 
-def formatCourseName(text):
+
+def format_course_name(text: str) -> str:
     if "-" in text:
         text = text.rpartition("-")[-1].strip()
     if "\n" in text:
         text = text.rpartition("\n")[-1].strip()
-    return formatDefault(text)
+    return format_default(text)
 
 
-def formatDefault(text):
-    text = removeNonPrintable(text)
+def format_default(text: str) -> str:
+    text = remove_non_printable_chars(text)
     if "\n" in text:
         text = text.rpartition("\n")[0]
     return text.replace("/", " ").replace("?", " ").replace("*", " ").strip()
 
 
-def removeNonPrintable(text):
-    printable = set(string.printable)
-    return ''.join(filter(lambda x: x in printable, text))
-        
+def remove_non_printable_chars(text: str) -> str:
+    printable_set = set(string.printable)
+    return ''.join(filter(lambda x: x in printable_set, text))
 
-def organizeFiles(courses, file_root):
-    allDownloadsFinished()
-    for course_name in courses:
-        course = courses[course_name]
 
-        #Create sub-folder for course
-        if len(course.file_groups) > 0:
-            course_path = file_root + course_name +"/"
+def organize_files(course_dict: Dict[str, Course], root_directory_name: str) -> None:
+    all_downloads_finished()
+    for course_name in course_dict:
+        course = course_dict[course_name]
+
+        # Create sub-folder for course
+        if course.file_groups:
+            course_path = root_directory_name + course_name + "/"
             if not os.path.isdir(course_path):
                 os.mkdir(course_path)
 
@@ -270,44 +278,52 @@ def organizeFiles(courses, file_root):
                     f = open(course_path + course_name + ".html", "w")
                     f.write(course.html)
                     f.close()
-        
+
             for group_name in course.file_groups:
                 file_group = course.file_groups[group_name]
-                
+
                 if not isinstance(file_group, list):
-                    moveFile(file_group, file_root, course_path)
-                
-                elif len(file_group) > 0:
+                    move_file(file_group, root_directory_name, course_path)
+
+                elif file_group:
                     group_path = course_path + group_name + "/"
                     if not os.path.isdir(group_path):
                         os.mkdir(group_path)
 
                     for file_name in file_group:
-                        moveFile(file_name, file_root, group_path)
+                        move_file(file_name, root_directory_name, group_path)
 
-                        
-def moveFile(file_name, file_root, group_path):
+
+def move_file(file_name: str, root_directory_name: str, group_path: str) -> None:
     try:
-        os.rename(file_root + file_name, group_path + file_name)
+        os.rename(root_directory_name + file_name, group_path + file_name)
     except FileNotFoundError:
-        file_name += "( \([0-9]+\))?"
-        
-        for f_name in os.listdir(file_root):
-            if re.match(file_name, f_name):
-                os.rename(file_root + f_name, group_path + f_name)
+        # TODO: should this be a raw string instead?
+        file_name_regex = file_name + "( \([0-9]+\))?"
+        for name in os.listdir(root_directory_name):
+            if re.match(file_name_regex, name):
+                os.rename(root_directory_name + name, group_path + name)
 
-                        
-## Main ##
 
-#Create driver and access Moodle
-driver = setupDriver(5)
-queryMoodle(driver)
-loginMoodle(driver, 1)
+# endregion Helper Methods
 
-#Visit each course and download all relevant files
-courses = scrapeCourseMetadata(driver, 1)
-courses = downloadCourseData(driver, courses)
+# region Main
 
-#Re-organize files
-organizeFiles(courses, "files/")
+# Create driver and access Moodle
 
+def main_function() -> None:
+    chrome_web_driver = setup_chrome_web_driver(5)
+    query_moodle(chrome_web_driver)
+    login_to_moodle(chrome_web_driver, 1)
+
+    # Visit each course and download all relevant files
+    courses = scrape_course_metadata(chrome_web_driver, 1)
+    courses = download_course_data(chrome_web_driver, courses)
+
+    # Re-organize files
+    organize_files(courses, "files/")
+
+
+main_function()
+
+# endregion Main
